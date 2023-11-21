c6589ee7013fc9c8ad548578387422c7eddb8096
Meht0k <115866935+Meht0k@users.noreply.github.com>
2023-11-21 12:32:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// var tl = gsap.timeline();
+
+
+gsap.to('.loader h1', {
+    scale: 1.1,
+    // delay:.5,
+    duration: 1,
+    onStart: loader("")
+})
+
+gsap.to('.loader', {
+    top: "-100vh",
+    delay: 2.5,
+    duration: 2,
+})
+
+function loader() {
+    var num = 0;
+    setInterval(function () {
+        num = num + Math.floor(Math.random() * 15);
+        if (num < 100) {
+            document.querySelector(".loader h1").innerHTML = num + "%";
+        } else {
+            num = 100;
+            document.querySelector(".loader h1").innerHTML = num + "%";
+        }
+    }, 150)
+}
+
+loader();
+
+
+
+
