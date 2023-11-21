b71c122e5688fe5d030144550cf6dfa51ce5f1cb
Meht0k <115866935+Meht0k@users.noreply.github.com>
2023-11-21 12:38:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ gsap.to('.loader h1', {
 
 gsap.to('.loader', {
     top: "-100vh",
-    delay: 2.5,
+    delay: 2.2,
     duration: 2,
 })
 
 function loader() {
     var num = 0;
     setInterval(function () {
-        num = num + Math.floor(Math.random() * 15);
+        num = num + Math.floor(Math.random() * 20);
         if (num < 100) {
             document.querySelector(".loader h1").innerHTML = num + "%";
         } else {
